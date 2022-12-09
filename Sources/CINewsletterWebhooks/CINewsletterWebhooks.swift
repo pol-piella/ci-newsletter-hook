8e867873399f3152d11c93f7ee057128efdf91b9
Pol Piella Abadia <hi@polpiella.dev>
2022-12-09 10:32:31 +0000

    Empty Swift package

---
+++ Sources/CINewsletterWebhooks/CINewsletterWebhooks.swift
@@ -0,0 +1,8 @@
+@main
+public struct CINewsletterWebhooks {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(CINewsletterWebhooks().text)
+    }
+}
